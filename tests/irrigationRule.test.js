016488ce3c064b7f05de864ed6e94a50eb5a50f9
Jonathas-Guimaraes <jonathasrmcguimaraes@gmail.com>
2026-04-04 16:27:16 -0300

    Primeiros arquivos

---
+++ tests/irrigationRule.test.js
@@ -0,0 +1,11 @@
+//Então abaixo na linha 2 seria o import do Java (Static Import?)
+const getAdvice = require("../backend/utils/irrigationRule");
+
+
+//No JEstm a própria função test é "a marcação @test do JUnity" pulando
+//a necessidade de uma função pra apenas executar a original.
+test("temperatura alta", () => { //"Descrição que vai aparecer no Log"
+//expect em JS é o Assert do Java, mas na ordem contrária
+//os parametros são (valor real/função).toBe(valor esperado)
+expect(getAdvice(35)).toBe("Irrigação URGENTE");
+});
